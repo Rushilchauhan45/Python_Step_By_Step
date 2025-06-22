0dc4c221bb69c063250f96d6967a936af0c10abb
Rushilchauhan45 <chauhanrushil45@gmail.com>
2025-06-22 22:29:35 +0530

    Week 1-Day 2 Complete

---
+++ Week-1/Programs/Demo.py
@@ -0,0 +1,10 @@
+
+
+#print Statement in Python 
+# The print statement is used to output data to the console.
+# Syntax: print(object(s), sep=' ', end='\n', file=sys.stdout, flush=False)
+# Example of print statement in Python
+
+
+#printf("Hello, world"); --->Wrong Syntax
+print("Hello, World!") # ---> Right Syntax
