721772fe1c07ebd0fd588b5b6184160faeb8ba86
Rushilchauhan45 <chauhanrushil45@gmail.com>
2025-06-23 16:09:19 +0530

    Day 3 of Week;

---
+++ Week-1/Programs/Input-Output.py
@@ -0,0 +1,20 @@
+
+
+#print Statement in Python 
+# The print statement is used to output data to the console.
+# Syntax: print(object(s), sep=' ', end='\n', file=sys.stdout, flush=False)
+# Example of print statement in Python
+
+
+
+print("Hello, Python world!")# ---> Right Syntax
+
+
+#input Statement in Python
+# The input statement is used to take input from the user.
+# Syntax: input(prompt)
+
+name = input("Enter your name: ")  # ---> Right Syntax
+print("Name is: ",name)
+print(name , "is good man!")
+
